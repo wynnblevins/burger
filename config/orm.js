5f6c944b4f582465f5a099b4a25888dc827f4097
Wynn Blevins <wynnblevins@yahoo.com>
2018-05-02 16:57:54 -0400

    Deleted previous work (aka what I did before I actually read the instructions).  Arranged project layout according to instructions.

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+var connection = require('connection.js');
+
+module.exports = {
+    selectAll: function () {
+        console.log('Inside selectAll method.');
+    },
+    
+    insertOne: function () {
+        console.log('Inside insertOne method.')
+    },
+
+    updateOne: function () {
+        console.log('Inside updateOne method.');    
+    }
+};
