8244ca264503c271ab6330789ca5186885c8a837
Wynn Blevins <wynnblevins@yahoo.com>
2018-05-03 18:02:09 -0400

    Set up /api/burgers PUT, POST, and GET endpoints.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,13 @@
 module.exports = function (app) {
-    
+    app.get('/api/burger', (req, res) => {
+        console.log('Inside burgers GET endpoint.');
+    });
+
+    app.post('/api/burger', (req, res) => {
+        console.log('Inside burgers POST endpoint.');
+    });
+
+    app.put('/api/burger', (req, res) => {
+        console.log('Inside burgers PUT endpoint');
+    });
 };
