a7f730c6b55dee6b0adec962376e8fdbfea485d8
Wynn Blevins <wynnblevins@yahoo.com>
2018-05-05 15:53:37 -0400

    Setting up config file for database passwords.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,11 +2,9 @@ const Burger = require('../models/burger.js');
 
 module.exports = function (app) {
     app.get('/api/burger', (req, res) => {
-        console.log('Inside burgers GET endpoint.');
         return Burger.findAll({}).then(function (burgers) {
-            console.log(burgers);
+            res.send(burgers);
         }); 
-    
     });
 
     app.post('/api/burger', (req, res) => {
--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,16 @@
 const Sequelize = require('sequelize');
 
-var connection = new Sequelize('burger_db', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-});
-
-var Burger = connection.define('burger', {
-    devoured: Sequelize.BOOLEAN,
-    burger_name: Sequelize.STRING
-}, {
-    timestamps: false
-});
-
-connection.sync();
-
-module.exports = Burger;
+module.exports = function(sequelize, DataTypes) {
+    var Burger = connection.define('burger', {
+        devoured: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        burger_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    }, {
+        timestamps: false
+    });
+};
