8996b51ed46f3d986685b2e5c21195f949e5b61e
Wynn Blevins <wynnblevins@yahoo.com>
2018-05-06 15:21:12 -0400

    Setting up heroku deployment.

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mysql = require("mysql");
-
 var app = express();
-
-
 var db = require('./models');
 
 // Set the port of our application
@@ -24,11 +21,19 @@ app.use('/public', express.static(__dirname + "/public"))
 require('./controllers/burgers_controller.js')(app);
 require('./controllers/html_endpoints.js')(app);
 
+// object that is used when determining whether to drop the DB
+// the database is not dropped by default... 
+var syncObj = { force: false };
+
+// check if argument for database drop exists
+const DROP_ARG_NDX = 2;
+if (process.argv[DROP_ARG_NDX] === 'freshDb') {
+  syncObj.force = true; 
+}
 
 // Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-    app.listen(PORT, function() {
-      console.log("App listening on PORT " + PORT);
-    });
+db.sequelize.sync(syncObj).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
