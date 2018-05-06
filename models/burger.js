ca4e09f9aa6576e369e9712625771e3388725e5f
Wynn Blevins <wynnblevins@yahoo.com>
2018-05-06 14:12:48 -0400

    Application now requires user to enter at least one letter into burger input

--- models/burger.js
+++ models/burger.js
@@ -7,6 +7,9 @@ module.exports = function(sequelize, DataTypes) {
         burger_name: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                len: [1]
+            }
         }
     }, {
         timestamps: false
