e266a9f9bbab4053488bdfd0ad39efa118921692
Wynn Blevins <wynnblevins@yahoo.com>
2018-05-05 21:10:43 -0400

    wrote code to append burgers and devour buttons to uneaten div section.  added an event handler that fires on button devour button click (currently logs a message to console confirming the button has been clicked).

--- public/assets/scripts/burgerScript.js
+++ public/assets/scripts/burgerScript.js
@@ -8,29 +8,45 @@
         var $burgerInput = $('#burgerInput');
         
         $('#addBurgerButton').on('click', function () {
-            console.log('Inside add burger frontend click event');
-
             $.post('http://localhost:8080/api/burger', {
                 burger_name: $burgerInput.val()
             }, function (data, status) {
-                console.log('Inside success handler of POST');
                 init();
             });  
         });
 
+        $('.burgersSection').on('click', 'button.devourButton', function () {
+            console.log('Inside devour button click method.');
+        }); 
+
         function createBurgerDiv(burger) {
             // decide where to append the burger div...
             if (burger.devoured) {
-                $eatenSection.append(`<div>${burger.burger_name}</div>`)
+                $eatenSection.append(`
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card card-body bg-light">${burger.burger_name}</div>
+                        </div>
+                    </div>
+                `);
             } else {
-                $uneatenSection.append(`<div>${burger.burger_name}</div>`)
+                $uneatenSection.append(`
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="card card-body bg-light">${burger.burger_name}</div>
+                        </div>    
+                        <div class="col-4">
+                            <button type="button" class="devourButton btn btn-default">Devour It!</button>
+                        </div>
+                    </div>        
+                `);
             }
         }
 
         function init() {
             $eatenSection.empty();
             $uneatenSection.empty();
-            
+
             $.get('http://localhost:8080/api/burger', function (data) {
                 for (var i = 0; i < data.length; i++) {
                     createBurgerDiv(data[i]);
