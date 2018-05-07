f32fb506b0f64d29eec1739f2bbafbaa9ff1dcd0
Wynn Blevins <wynnblevins@yahoo.com>
2018-05-07 13:29:49 -0400

    Attempting to deploy to heroku

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE IF NOT EXISTS burger_db;
 USE burger_db;
+
+CREATE TABLE Burgers (
+	id INT NOT NULL AUTO_INCREMENT, 
+    devoured BOOLEAN, 
+    burger_name VARCHAR(255),
+    PRIMARY KEY (id)
+);
